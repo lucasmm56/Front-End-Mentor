4b56ff1f91daec5c8364b64f1e64dc6ba0249c96
lucasmm56 <lucasmm56@gmail.com>
2023-03-13 22:21:37 -0300

    novo projeto

---
+++ visualizacao-do-artigo/js/script.js
@@ -0,0 +1,14 @@
+var divshared = document.querySelector('.otherone');
+var divAside = document.querySelector('.aside');
+
+console.log(divshared);
+
+function shared(){
+    if(divAside.style.display == 'none'){
+        divAside.style.display = 'block';
+        divshared.style.display = 'none';
+    }else{
+        divAside.style.display = 'none';
+        divshared.style.display = 'block';
+    }
+}
